40c3c3e7bd603ceda5f229cc70ec932bfe746610
Rohan Sinha <rohan.sinha16@gmail.com>
2013-08-02 09:38:22 -0500

    first commit

---
+++ app/models/student.rb
@@ -0,0 +1,3 @@
+class Student < ActiveRecord::Base
+  attr_accessible :class_id, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentDirectory::Application.config.secret_token = '99524639f85a5fe3c1f1b560468b0b9eb581d8dda3c512b8d9535ef4a4dc12dfcd996b5971a27a2ca48ff84d1f2d9428cf2fac0b7ac526a1efe9d0d51b76d75c'
